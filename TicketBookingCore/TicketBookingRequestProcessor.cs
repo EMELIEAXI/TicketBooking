775b25c2b183f012d5c132eb4cbcdab60230ae23
Emelie <emelieaxi@hotmail.com>
2025-01-30 11:37:07 +0100

    Passed test

--- TicketBookingCore/TicketBookingRequestProcessor.cs
+++ TicketBookingCore/TicketBookingRequestProcessor.cs
@@ -1,18 +1,30 @@
 ﻿
+using System.Runtime.CompilerServices;
+
 namespace TicketBookingCore.Tests
 {
     public class TicketBookingRequestProcessor
     {
+        private readonly ITicketBookingRepository _ticketBookingRepository;
         public TicketBookingRequestProcessor(ITicketBookingRepository ticketBookingRepository)
         {
+            _ticketBookingRepository = ticketBookingRepository;
         }
 
         public TicketBookingResonse Book(TicketBookingRequest request)
-        {
+        { 
             if (request is null)
             {
                 throw new ArgumentNullException(nameof(request));
             }
+
+            _ticketBookingRepository.Save(new TicketBooking
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+            });
+
             return new TicketBookingResonse
             {
                 FirstName = request.FirstName,
